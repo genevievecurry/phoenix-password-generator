87f7a240153df717652b051e9c28a3862ee7d968
Genevieve Curry <gcurry@protonmail.com>
2021-06-08 15:11:54 -0700

    Tweaks to prod config.

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pw_app, PwAppWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "https://gross-warped-trogon.gigalixirapp.com/", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
