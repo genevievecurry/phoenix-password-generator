2d017010a3d028de8b37230c779ced37bbf8b83b
Genevieve Curry <gcurry@protonmail.com>
2023-05-24 12:13:56 -0700

    Troubleshooting deployment setup

--- config/prod.exs
+++ config/prod.exs
@@ -54,6 +54,12 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 # import_config "prod.secret.exs"
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 host = System.get_env("PHX_HOST") || "example.com"
 
@@ -61,5 +67,5 @@ config :pw_app, PwAppWeb.Endpoint,
   # Possibly not needed, but doesn't hurt
   http: [port: {:system, "PORT"}],
   url: [host: host, port: 443],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  secret_key_base: secret_key_base,
   server: true
